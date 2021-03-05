7396233d96375bbefb2c7a6043cd60416fb95428
BullEKorV <elias.wasielewski@elev.ga.ntig.se>
2021-03-05 15:43:12 +0100

    massor av skit har försvunnit...

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const snake_border = 'darkblue';
 const snakeboard = document.getElementById("gameCanvas");
 const snakeboard_ctx = gameCanvas.getContext("2d");
 
+
 let score = 0;
 
 let cellSize = 70;
@@ -17,11 +18,11 @@ snakeboard.height = boardSizeY;
 
 let snakeSpeed = 250;
 
-let snake = [  
-    {x: boardSizeX / 2, y: boardSizeY / 2},  
-    {x: boardSizeX / 2 - cellSize, y: boardSizeY / 2},  
-    {x: boardSizeX / 2 -cellSize*2, y: boardSizeY / 2},  
-    {x: boardSizeX / 2 -cellSize*3, y: boardSizeY / 2},  
+let snake = [
+    { x: boardSizeX / 2, y: boardSizeY / 2 },
+    { x: boardSizeX / 2 - cellSize, y: boardSizeY / 2 },
+    { x: boardSizeX / 2 - cellSize * 2, y: boardSizeY / 2 },
+    { x: boardSizeX / 2 - cellSize * 3, y: boardSizeY / 2 },
 ];
 
 let changingDirection = false;
@@ -36,39 +37,40 @@ let currentGamemode = "Normal";
 document.getElementById('current-gamemode').innerHTML = currentGamemode;
 
 function gamemodeEasier() {
-    if(currentGamemode == "Normal") {
-        currentGamemode = "Easy"; 
+    if (currentGamemode == "Normal") {
+        currentGamemode = "Easy";
     }
-    else if(currentGamemode == "Easy") {
-        currentGamemode = "Hard"; 
+    else if (currentGamemode == "Easy") {
+        currentGamemode = "Hard";
     }
-    else if(currentGamemode == "Hard") {
-        currentGamemode = "Normal";   
+    else if (currentGamemode == "Hard") {
+        currentGamemode = "Normal";
     }
     document.getElementById('current-gamemode').innerHTML = currentGamemode;
 }
 
+
 function gamemodeHarder() {
-    if(currentGamemode == "Normal") {
-        currentGamemode = "Hard"; 
+    if (currentGamemode == "Normal") {
+        currentGamemode = "Hard";
     }
-    else if(currentGamemode == "Hard") {
-        currentGamemode = "Easy"; 
+    else if (currentGamemode == "Hard") {
+        currentGamemode = "Easy";
     }
-    else if(currentGamemode == "Easy") {
-        currentGamemode = "Normal";   
+    else if (currentGamemode == "Easy") {
+        currentGamemode = "Normal";
     }
     document.getElementById('current-gamemode').innerHTML = currentGamemode;
 }
 
 function startGame() {
-    if(currentGamemode == "Easy") {
+    if (currentGamemode == "Easy") {
         snakeSpeed = 220
     }
-    else if(currentGamemode == "Normal") {
+    else if (currentGamemode == "Normal") {
         snakeSpeed = 150;
     }
-    else if(currentGamemode == "Hard") {
+    else if (currentGamemode == "Hard") {
         snakeSpeed = 80;
     }
     main();
@@ -82,7 +84,7 @@ function main() {
     if (checkCollision()) return;
     changingDirection = false;
     setTimeout(function onTick() {
-        if(keyBacklog != 0) changeDirection();
+        if (keyBacklog != 0) changeDirection();
         clearCanvas();
         drawFood();
         moveSnake();
@@ -91,24 +93,19 @@ function main() {
     }, snakeSpeed)
 }
 function clearCanvas() {
-    //  Select the colour to fill the drawing
     snakeboard_ctx.fillStyle = board_background;
-    //  Select the colour for the border of the canvas
     snakeboard_ctx.strokestyle = board_border;
-    // Draw a "filled" rectangle to cover the entire canvas
     snakeboard_ctx.fillRect(0, 0, boardSizeX, boardSizeY);
-    // Draw a "border" around the entire canvas
     snakeboard_ctx.strokeRect(0, 0, boardSizeX, boardSizeY);
-  }
+}
 
-function drawSnake() 
-{  
+function drawSnake() {
     const headSize = 0.6;
     const snakeSize = 0.5;
 
-    snakeboard_ctx.fillStyle = 'lightgreen';  
+    snakeboard_ctx.fillStyle = 'lightgreen';
     snakeboard_ctx.strokestyle = 'black';
-    snakeboard_ctx.fillRect(snake[0].x + (cellSize - (cellSize * headSize)) * 0.5 , snake[0].y + (cellSize - (cellSize * headSize)) * 0.5, cellSize * headSize, cellSize * headSize);  
+    snakeboard_ctx.fillRect(snake[0].x + (cellSize - (cellSize * headSize)) * 0.5, snake[0].y + (cellSize - (cellSize * headSize)) * 0.5, cellSize * headSize, cellSize * headSize);
     //snakeboard_ctx.strokeRect(snake[0].x + (cellSize - (cellSize * headSize)) * 0.5 , snake[0].y + (cellSize - (cellSize * headSize)) * 0.5, cellSize * headSize, cellSize * headSize);
 
     for (let i = 0; i < snake.length; i++) {
@@ -116,74 +113,73 @@ function drawSnake()
         let yCord = snake[i].y + (cellSize - (cellSize * snakeSize)) * 0.5;
         let width = cellSize * snakeSize;
         let height = cellSize * snakeSize;
-        snakeboard_ctx.fillStyle = 'lightgreen';  
+        snakeboard_ctx.fillStyle = 'lightgreen';
         if (i > 0) {
-            if(snake[i].x < snake[i - 1].x) {
-                width = (cellSize - (cellSize * snakeSize)) * 0.5 + cellSize *snakeSize;
+            if (snake[i].x < snake[i - 1].x) {
+                width = (cellSize - (cellSize * snakeSize)) * 0.5 + cellSize * snakeSize;
                 if (snake[i - 1].x - snake[i].x > cellSize) xCord = 0;
             }
-            if (snake[i].x > snake[i - 1].x){
+            if (snake[i].x > snake[i - 1].x) {
                 xCord = snake[i].x;
                 width = (cellSize - (cellSize * snakeSize)) * 0.5 + cellSize * snakeSize;
                 if (snake[i].x - snake[i - 1].x > cellSize) xCord = snake[i].x + (cellSize - (cellSize * snakeSize)) * 0.5;
             }
-            
-            if( snake[i].y < snake[i - 1].y) {
-                height = (cellSize - (cellSize * snakeSize)) * 0.5 + cellSize *snakeSize;
+
+            if (snake[i].y < snake[i - 1].y) {
+                height = (cellSize - (cellSize * snakeSize)) * 0.5 + cellSize * snakeSize;
                 if (snake[i - 1].y - snake[i].y > cellSize) yCord = 0;
             }
-            if (snake[i].y > snake[i - 1].y){
+            if (snake[i].y > snake[i - 1].y) {
                 yCord = snake[i].y;
-                height = (cellSize - (cellSize * snakeSize)) * 0.5 + cellSize *snakeSize;
+                height = (cellSize - (cellSize * snakeSize)) * 0.5 + cellSize * snakeSize;
                 if (snake[i].y - snake[i - 1].y > cellSize) yCord = snake[i].y + (cellSize - (cellSize * snakeSize)) * 0.5;
             }
-        } 
-        snakeboard_ctx.fillRect(xCord, yCord, width, height);  
-        
+        }
+        snakeboard_ctx.fillRect(xCord, yCord, width, height);
+
         xCord = snake[i].x + (cellSize - (cellSize * snakeSize)) * 0.5;
         yCord = snake[i].y + (cellSize - (cellSize * snakeSize)) * 0.5;
         width = cellSize * snakeSize;
         height = cellSize * snakeSize;
         if (i < snake.length - 1) {
-            if(snake[i].x < snake[i + 1].x) {
-                width = (cellSize - (cellSize * snakeSize)) * 0.5 + cellSize *snakeSize;
+            if (snake[i].x < snake[i + 1].x) {
+                width = (cellSize - (cellSize * snakeSize)) * 0.5 + cellSize * snakeSize;
                 if (snake[i + 1].x - snake[i].x > cellSize) xCord = 0;
             }
-            if (snake[i].x > snake[i + 1].x){
+            if (snake[i].x > snake[i + 1].x) {
                 xCord = snake[i].x;
-                width = (cellSize - (cellSize * snakeSize)) * 0.5 + cellSize *snakeSize;
+                width = (cellSize - (cellSize * snakeSize)) * 0.5 + cellSize * snakeSize;
                 if (snake[i].x - snake[i + 1].x > cellSize) xCord = snake[i].x + (cellSize - (cellSize * snakeSize)) * 0.5;
             }
-            
-            if(snake[i].y < snake[i + 1].y) {
-                height = (cellSize - (cellSize * snakeSize)) * 0.5 + cellSize *snakeSize;
+
+            if (snake[i].y < snake[i + 1].y) {
+                height = (cellSize - (cellSize * snakeSize)) * 0.5 + cellSize * snakeSize;
                 if (snake[i + 1].y - snake[i].y > cellSize) yCord = 0;
             }
-            if (snake[i].y > snake[i + 1].y){
+            if (snake[i].y > snake[i + 1].y) {
                 yCord = snake[i].y;
-                height = (cellSize - (cellSize * snakeSize)) * 0.5 + cellSize *snakeSize;
+                height = (cellSize - (cellSize * snakeSize)) * 0.5 + cellSize * snakeSize;
                 if (snake[i].y - snake[i + 1].y > cellSize) yCord = snake[i].y + (cellSize - (cellSize * snakeSize)) * 0.5;
             }
         }
-        snakeboard_ctx.fillRect(xCord, yCord, width, height);  
+        snakeboard_ctx.fillRect(xCord, yCord, width, height);
     }
 }
 
-function moveSnake() 
-{  
-  const head = {x: snake[0].x + xVel, y: snake[0].y + yVel};
-  snake.unshift(head);
-  checkCollision();
+function moveSnake() {
+    const head = { x: snake[0].x + xVel, y: snake[0].y + yVel };
+    snake.unshift(head);
+    checkCollision();
 
-  const hasEatenFood = snake[0].x === foodX && snake[0].y === foodY;
-  if (hasEatenFood) {
-      generateFood();
-      score++;
-      document.getElementById('score').innerHTML = score;
+    const hasEatenFood = snake[0].x === foodX && snake[0].y === foodY;
+    if (hasEatenFood) {
+        generateFood();
+        score++;
+        document.getElementById('score').innerHTML = score;
 
-  } else {
-      snake.pop();
-  }
+    } else {
+        snake.pop();
+    }
 }
 
 function checkCollision() {
@@ -192,25 +188,25 @@ function checkCollision() {
     }
 
     snake.forEach(function isOutsideBoard(part) {
-        if(part.x >= boardSizeX) part.x = 0;
-        if(part.x <= -cellSize) part.x = boardSizeX -cellSize;
-        if(part.y >= boardSizeY) part.y = 0;
-        if(part.y <= -cellSize) part.y = boardSizeY - cellSize;
+        if (part.x >= boardSizeX) part.x = 0;
+        if (part.x <= -cellSize) part.x = boardSizeX - cellSize;
+        if (part.y >= boardSizeY) part.y = 0;
+        if (part.y <= -cellSize) part.y = boardSizeY - cellSize;
     });
     return false;
 }
 
 function changeDirection(event) {
     let keyPressed;
-    if (event){
-         keyPressed = event.keyCode;
-         keyBacklog = 0;
+    if (event) {
+        keyPressed = event.keyCode;
+        keyBacklog = 0;
     }
     else keyPressed = keyBacklog;
-    if (changingDirection){
+    if (changingDirection) {
         keyBacklog = keyPressed;
         return;
-    }    
+    }
     changingDirection = true;
     keyBacklog = 0;
     updateDirection(keyPressed);
@@ -220,26 +216,26 @@ function updateDirection(keyPressed) {
     const RIGHT_KEY = 39;
     const UP_KEY = 38;
     const DOWN_KEY = 40;
-    if(keyPressed === LEFT_KEY && xVel !== cellSize) {
+    if (keyPressed === LEFT_KEY && xVel !== cellSize) {
         xVel = -cellSize;
         yVel = 0;
     }
-    if(keyPressed === RIGHT_KEY && xVel !== -cellSize) {
+    if (keyPressed === RIGHT_KEY && xVel !== -cellSize) {
         xVel = cellSize;
         yVel = 0;
     }
-    if(keyPressed === UP_KEY && yVel !== cellSize) {
+    if (keyPressed === UP_KEY && yVel !== cellSize) {
         xVel = 0;
         yVel = -cellSize;
     }
-    if(keyPressed === DOWN_KEY && yVel !== -cellSize) {
+    if (keyPressed === DOWN_KEY && yVel !== -cellSize) {
         xVel = 0;
         yVel = cellSize;
     }
 }
 
-function randomCords(min,max) {
-    return Math.round((Math.random() * (max-min) + min) / cellSize) * cellSize;
+function randomCords(min, max) {
+    return Math.round((Math.random() * (max - min) + min) / cellSize) * cellSize;
 }
 function generateFood() {
     foodX = randomCords(0, boardSizeX - cellSize);
@@ -256,4 +252,4 @@ function drawFood() {
     snakeboard_ctx.strokestyle = 'darkgreen';
     snakeboard_ctx.fillRect(foodX + (cellSize - (cellSize * foodSize)) * 0.5, foodY + (cellSize - (cellSize * foodSize)) * 0.5, cellSize * foodSize, cellSize * foodSize);
     snakeboard_ctx.strokeRect(foodX + (cellSize - (cellSize * foodSize)) * 0.5, foodY + (cellSize - (cellSize * foodSize)) * 0.5, cellSize * foodSize, cellSize * foodSize);
-  }
+}
